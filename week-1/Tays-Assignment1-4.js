9bfc2a9e07c938650eb3cbc88db75796bd4db2f2
David Tays <davidtays22@gmail.com>
2017-11-26 22:15:24 -0600

    adding week-1

---
+++ week-1/Tays-Assignment1-4.js
@@ -0,0 +1,28 @@
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName = "David";
+var lastName = "Tays";
+var age = 35;
+var street = "55 Sesame St.";
+var city = "Pleasantville";
+var state = "Neverland";
+var zipCode = 98765;
+
+// output 
+console.log(firstName + '\n' + lastName + '\n' + age + '\n' + street + '\n' + city + '\n' + state + '\n' + zipCode); 
+
+
+// end program 
