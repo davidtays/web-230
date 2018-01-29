2b41a9049ceaf465961dd6f7dddfc4ed2e172230
David Tays <davidtays22@gmail.com>
2018-01-28 23:01:15 -0600

    add week 7

---
+++ week-7/Tays_ex7-2.js
@@ -0,0 +1,41 @@
+var header = require('../header.js');
+
+/*
+ Expected output:
+
+ FirstName LastName
+ Exercise 7.2
+ Today's Date
+
+ 1 Thomas Edison Software Engineer
+ 2 Benjamin Franklin Programmer
+ 3 Nikola Tesla Project Manager
+ 4 Charles Babbage Product Manager
+ 5 Alexander Bell Business Analyst
+
+ */
+
+// start program
+
+function Employee(eID,fName,lName,eTitle){
+    this.id = eID;
+    this.firstName = fName;
+    this.lastName = lName;
+    this.title = eTitle;
+}
+var emps = [
+    new Employee(1001, "Thomas", "Edison", "Software Engineer"),
+    new Employee(1002, "Benjamin", "Franklin", "Programmer"),
+    new Employee(1003, "Nikola", "Tesla", "Project Manager"),
+    new Employee(1003, "Charles", "Babbage", "Product Manager"),
+    new Employee(1004, "Alexander", "Bell", "Business Analyst")
+];
+
+
+
+//output
+Object.entries(emps).forEach(
+    ([index, key]) => console.log(key.firstName + " " + key.lastName + " " + key.title)
+);
+
+// end program
